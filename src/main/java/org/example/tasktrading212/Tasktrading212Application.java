3c14f35697b3ca96b704999ef5a17569d2cb6dc9
sstefanov <84671344+sstefanov19@users.noreply.github.com>
2026-01-27 14:23:28 +0200

    init commit

---
+++ src/main/java/org/example/tasktrading212/Tasktrading212Application.java
@@ -0,0 +1,13 @@
+package org.example.tasktrading212;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tasktrading212Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tasktrading212Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/tasktrading212/Tasktrading212ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tasktrading212;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tasktrading212ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
